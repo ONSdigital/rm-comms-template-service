3c276f234b7f82d3fa55fc7d7b313d28e56428fd
Joseph Walton <jwgwalton@gmail.com>
2017-12-01 13:52:56 +0000

    Removes tox-docker and corrects travis build

--- tests/test_client.py
+++ tests/test_client.py
@@ -7,9 +7,3 @@ class TestClient(TestCase):
     @staticmethod
     def create_app():
         return create_app("tests.config.TestConfig")
-
-    @staticmethod
-    def tearDown():
-        # TODO: TRUNCATE POSTGRES TABLES IN DOCKER CONTAINER,
-        # TODO: also relies on docker container being up, could start it in startup (needs to really be clean)
-        pass
