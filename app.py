b7fb85fc63b665ad707d1815f32c70df3ea6b080
Joseph Walton <jwgwalton@gmail.com>
2018-03-06 12:53:34 +0000

    Add retry to making DB connection

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from run import create_app, configure_structlogger
+import logging
+import structlog
+
+from retrying import RetryError
+from run import create_app, configure_structlogger, initialise_db
 
 # This is a duplicate of run.py, with minor modifications to support gunicorn execution.
+
+logger = structlog.wrap_logger(logging.getLogger(__name__))
 
 config_path = "config.Config"
 
 app = create_app(config_path)
 
 configure_structlogger(app.config)
+
+try:
+    initialise_db(app)
+except RetryError:
+    logger.exception('Failed to initialise database')
+    exit(1)
--- run.py
+++ run.py
@@ -1,26 +1,22 @@
+import logging
+import structlog
+
 from flask_cors import CORS
 from flask import Flask
 from sqlalchemy import event, DDL
+from sqlalchemy.exc import ProgrammingError, DatabaseError
+from retrying import RetryError, retry
 
 from application.utils.logging import configure_structlogger
 from application.models.models import db
+
+logger = structlog.wrap_logger(logging.getLogger(__name__))
 
 
 def create_app(config_path):
     # create and configure the Flask app
     app = Flask(__name__)
     app.config.from_object(config_path)
-
-    from application.models.models import CommunicationTemplate, CommunicationType # NOQA  # pylint: disable=wrong-import-position
-    from application.models.classification_type import ClassificationType # NOQA  # pylint: disable=wrong-import-position
-
-    # Set up database
-    with app.app_context():
-        db.init_app(app)
-        # Creates the schema, can't create the tables otherwise
-        event.listen(db.metadata, 'before_create', DDL("CREATE SCHEMA IF NOT EXISTS templatesvc"))
-        # Creates the tables from the models
-        db.create_all()
 
     # register view blueprints
     from application.views.info_view import info_view
@@ -36,12 +32,37 @@ def create_app(config_path):
     return app
 
 
+def retry_if_database_error(exception):
+    logger.error(exception)
+    return isinstance(exception, DatabaseError) and not isinstance(exception, ProgrammingError)
+
+
+@retry(retry_on_exception=retry_if_database_error, wait_fixed=2000, stop_max_delay=30000, wrap_exception=True)
+def initialise_db(app):
+    from application.models.models import CommunicationTemplate, CommunicationType # NOQA  # pylint: disable=wrong-import-position
+    from application.models.classification_type import ClassificationType # NOQA  # pylint: disable=wrong-import-position
+
+    # Set up database
+    with app.app_context():
+        db.init_app(app)
+        # Creates the schema, can't create the tables otherwise
+        event.listen(db.metadata, 'before_create', DDL("CREATE SCHEMA IF NOT EXISTS templatesvc"))
+        # Creates the tables from the models
+        db.create_all()
+
+
 if __name__ == '__main__':
     config_path = "config.Config"
 
     app = create_app(config_path)
 
     configure_structlogger(app.config)
+
+    try:
+        initialise_db(app)
+    except RetryError:
+        logger.exception('Failed to initialise database')
+        exit(1)
 
     host, port = app.config['HOST'], int(app.config['PORT'])
 
